18901ab515aed087578da9de147b5e48b67c6978
fd-Martin <fdmosharof@gmail.com>
2023-05-19 20:45:54 +0600

    70-3 Create checkout Form and load single service data

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 // console.log(process.env.DB_PASS)
 // console.log()
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.azm8txw.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -35,6 +35,17 @@ async function run() {
             const cursor = serviceCollection.find();
             const result = await cursor.toArray();
             res.send(result);
+        })
+
+        app.get('/services/:id',async(req,res)=>{
+            const id=req.params.id;
+            const query={_id: new ObjectId(id)}
+            const options={
+                projection:{title:1, price:1,service_id:1,}
+            }
+            const result=await serviceCollection.findOne(query,options);
+            res.send(result);
+
         })
 
         // Send a ping to confirm a successful connection
